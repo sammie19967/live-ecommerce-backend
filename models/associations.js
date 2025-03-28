415e307192240dd7862310b711f28d23dc41698d
Dev. Sammie Barasa <sammie.barasa@students.jkuat.ac.ke>
2025-03-28 05:46:33 +0300

    addedd asociation between users and posts

--- models/associations.js
+++ models/associations.js
@@ -1,6 +1,7 @@
 import User from './User.js';
 import LiveStream from './LiveStream.js';
 import LiveView from './LiveView.js';
+import Post from './Post.js'; // Import the Post model
 
 // Define Relationships
 User.hasMany(LiveStream, { foreignKey: 'sellerId', onDelete: 'CASCADE' });
@@ -12,4 +13,8 @@ LiveView.belongsTo(LiveStream, { foreignKey: 'streamId' });
 User.hasMany(LiveView, { foreignKey: 'userId', onDelete: 'CASCADE' });
 LiveView.belongsTo(User, { foreignKey: 'userId' });
 
-export { User, LiveStream, LiveView };
+// Association between User and Post
+User.hasMany(Post, { foreignKey: 'userId', onDelete: 'CASCADE' }); // A User can have many Posts
+Post.belongsTo(User, { foreignKey: 'userId' }); // A Post belongs to a User
+
+export { User, LiveStream, LiveView, Post };
